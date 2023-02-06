f42d791beadcae2f5939f6ebd6ce8314403d79dc
michael menebraye <michael@dufuna.org>
2023-02-06 14:29:56 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>M3 Website</h1>
+        <button type="button" class="btn btn-primary">Welcome</button>
       </header>
     </div>
   );
